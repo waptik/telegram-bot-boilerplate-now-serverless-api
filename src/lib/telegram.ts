a28e30195603ea15c473dba02a494b0311237189
waptik <itswaptik+github@gmail.com>
2020-05-13 20:28:40 +0000

    added lib back

---
+++ src/lib/telegram.ts
@@ -0,0 +1,17 @@
+import {Context as TelegrafContext, Extra} from 'telegraf';
+import {ExtraReplyMessage} from 'telegraf/typings/telegram-types';
+
+export function toArgs(ctx: TelegrafContext) {
+  const regex = /^\/([^@\s]+)@?(?:(\S+)|)\s?([\s\S]+)?$/i;
+  const parts = regex.exec(ctx.message!.text!.trim());
+  if (!parts) {
+    return [];
+  }
+  return !parts[3] ? [] : parts[3].split(/\s+/).filter((arg) => arg.length);
+}
+
+export const MARKDOWN = Extra.markdown(true) as ExtraReplyMessage;
+
+export const NO_PREVIEW = Extra.markdown(true).webPreview(false) as ExtraReplyMessage;
+
+export const hiddenCharacter = '\u200b';
